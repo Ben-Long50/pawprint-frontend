c4d8edd7ec3b9716eae78872db9c23376ff56029
Ben-Long50 <benjlong50@gmail.com>
2025-05-02 14:38:36 -0700

    redesigned landing puage

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -2,7 +2,6 @@ import { Link } from 'react-router-dom';
 import PawIcon from '../assets/PawIcon';
 import Button from './Button';
 import Logo from './Logo';
-import ProfilePic from './ProfilePic';
 import { mdiChevronDoubleDown } from '@mdi/js';
 import Icon from '@mdi/react';
 import { useRef, useState } from 'react';
@@ -69,81 +68,95 @@ const LandingPage = () => {
           />
         </div>
       </div>
-      <section className="mt-20 flex flex-col gap-24 overflow-x-hidden py-8 text-lg md:mt-0 md:gap-32 md:text-3xl">
-        <div
-          ref={infoRef1}
-          className={`${showInfoRef1 ? 'fade-in-infoRight' : 'opacity-0'} flex flex-col gap-6`}
-        >
-          <h2 className="text-primary mr-4 text-right font-logo text-3xl md:mb-4 md:mr-12 md:text-5xl">
-            A pet focused community
-          </h2>
-          <div className="md:grid md:grid-cols-1/3">
-            <div
-              className={`bg-secondary-2 col-start-2 ml-4 flex w-auto items-center gap-6 rounded-l-full p-4 shadow-md shadow-gray-300 md:gap-20 md:p-8 dark:shadow-black`}
-            >
-              <LandingPageImage image="/cat.JPG" className="w-48 md:size-96" />
-              <div>
-                <p className="text-secondary leading-normal">
-                  Join the social media community where your pets are the star!
-                </p>
-                <p className="text-tertiary mt-2 text-sm md:mt-6 md:text-2xl">
+      <section className="mx-auto mt-20 flex max-w-5xl flex-col py-8 text-lg max-md:p-4 md:mt-0 md:gap-40 md:text-3xl">
+        <div ref={infoRef1} className={`flex flex-col gap-8`}>
+          <div
+            className={`${showInfoRef1 ? 'fade-in-infoRight' : 'opacity-0'} bg-secondary-2 col-start-2 flex w-full flex-col items-start gap-4 self-end overflow-hidden rounded-xl p-4 shadow-md shadow-gray-300 md:w-3/4 md:p-6 dark:shadow-black`}
+          >
+            <div className="flex items-center gap-4 md:gap-10">
+              <h3 className="text-primary font-logo text-3xl md:text-4xl">
+                A pet focused community
+              </h3>
+              <PawIcon className="size-10 md:size-14" />
+            </div>
+            <div className="bg-secondary rounded-lg p-2 text-xl shadow-md shadow-gray-300 md:p-4 md:text-2xl dark:shadow-black">
+              <p className="text-secondary leading-normal">
+                Join the social media community where your pets are the star!{' '}
+                <span className="text-tertiary mt-2 text-sm md:mt-6 md:text-xl">
                   (Please keep posts on theme)
-                </p>
-              </div>
+                </span>
+              </p>
             </div>
           </div>
+          <div className="-gap-8 grid max-md:grid-rows-3 md:grid-cols-3">
+            <LandingPageImage
+              className={`${showInfoRef1 ? 'opacity-100 md:translate-x-8' : 'opacity-0 max-md:translate-y-16 md:translate-x-[15rem]'} z-10 transition-all delay-[.5s] duration-[2s] max-md:justify-self-start md:w-full`}
+              image="https://res.cloudinary.com/dm4tmla72/image/upload/v1731182849/pawprint/twohmchvwb9apit5izzd.jpg"
+            />
+            <LandingPageImage
+              className={`${showInfoRef1 ? 'opacity-100 max-md:-translate-y-16 md:translate-x-0' : 'opacity-0 max-md:translate-y-16 md:translate-x-[17rem]'} z-20 transition-all delay-[1s] duration-[2s] max-md:justify-self-center md:w-full md:translate-y-8`}
+              image="https://res.cloudinary.com/dm4tmla72/image/upload/v1734901118/pawprint/vhcllqkmbqttry4omf3n.jpg"
+            />
+            <LandingPageImage
+              className={`${showInfoRef1 ? 'opacity-100 max-md:-translate-y-32 md:-translate-x-8' : 'opacity-0 max-md:translate-y-0 md:translate-x-[19rem]'} z-30 transition-all delay-[1.5s] duration-[2s] max-md:justify-self-end md:w-full md:translate-y-16`}
+              image="https://res.cloudinary.com/dm4tmla72/image/upload/v1734900914/pawprint/cuh7pqcnxbtboympykha.jpg"
+            />
+          </div>
         </div>
-        <div
-          ref={infoRef2}
-          className={`${showInfoRef2 ? 'fade-in-infoLeft' : 'opacity-0'} flex flex-col gap-6`}
-        >
-          <h2 className="text-primary ml-4 text-left font-logo text-3xl md:mb-4 md:ml-12 md:text-5xl">
-            One account, many pets
-          </h2>
-          <div className="md:grid md:grid-cols-3/1">
-            <div className="fade-in-left bg-secondary-2 col-start-1 mr-4 flex items-center justify-end gap-6 rounded-r-full p-4 pr-8 shadow-md shadow-gray-300 md:gap-20 md:p-8 md:pr-20 dark:shadow-black">
+        <div ref={infoRef2} className={`flex flex-col gap-8`}>
+          <div
+            className={`${showInfoRef2 ? 'fade-in-infoLeft' : 'opacity-0'} bg-secondary-2 col-start-2 flex w-full flex-col items-start gap-4 self-start overflow-hidden rounded-xl p-4 shadow-md shadow-gray-300 md:w-3/4 md:p-8 dark:shadow-black`}
+          >
+            <div className="flex items-center gap-4 md:gap-8">
+              <PawIcon className="size-10 md:size-14" />
+              <h3 className="text-primary font-logo text-3xl md:mb-4 md:mr-12 md:text-4xl">
+                One account, many pets
+              </h3>
+            </div>
+            <div className="bg-secondary rounded-lg p-2 text-xl shadow-md shadow-gray-300 md:p-4 md:text-2xl dark:shadow-black">
               <p className="text-secondary leading-normal">
                 With one account you can manage multiple profiles each dedicated
                 to a different pet
               </p>
-              <div className="flex flex-col items-center justify-center gap-2">
-                <div className="flex justify-end gap-4 md:gap-8">
-                  <LandingPageImage
-                    image="/turtle.JPG"
-                    className="size-20 md:size-48"
-                  />
-                  <LandingPageImage
-                    image="/dog.PNG"
-                    className="size-20 md:size-48"
-                  />
-                </div>
-                <LandingPageImage
-                  image="/cat2.jpg"
-                  className="size-20 md:size-48"
-                />
-              </div>
             </div>
+          </div>
+          <div className="grid max-md:grid-rows-3 md:grid-cols-3">
+            <LandingPageImage
+              className={`${showInfoRef2 ? 'opacity-100 md:translate-x-8' : 'opacity-0 max-md:translate-y-16 md:-translate-x-[15rem]'} z-30 transition-all delay-[1.5s] duration-[2s] max-md:z-10 max-md:justify-self-end max-md:delay-[.5s] md:w-full md:translate-y-16`}
+              image="https://res.cloudinary.com/dm4tmla72/image/upload/v1731620285/pawprint/nvvoodlb3dhekixmoj5y.jpg"
+            />
+            <LandingPageImage
+              className={`${showInfoRef2 ? 'opacity-100 max-md:-translate-y-16 md:translate-x-0' : 'opacity-0 max-md:translate-y-16 md:-translate-x-[17rem]'} " z-20 transition-all delay-[1s] duration-[2s] max-md:justify-self-center md:w-full md:translate-y-8`}
+              image="https://res.cloudinary.com/dm4tmla72/image/upload/v1731362333/pawprint/kwudu6g20ctl96j2q3sp.jpg"
+            />
+            <LandingPageImage
+              className={`${showInfoRef2 ? 'opacity-100 max-md:-translate-y-32 md:translate-x-8' : 'opacity-0 max-md:translate-y-0 md:-translate-x-[19rem]'} z-10 transition-all delay-[.5s] duration-[2s] max-md:z-30 max-md:justify-self-start max-md:delay-[1.5s] md:w-full md:-translate-x-8`}
+              image="https://res.cloudinary.com/dm4tmla72/image/upload/v1731094920/pawprint/h8crzzbvtooet29abwnm.jpg"
+            />
           </div>
         </div>
         <div
           ref={infoRef3}
-          className={`${showInfoRef3 ? 'fade-in-infoRight' : 'opacity-0'} flex flex-col gap-6`}
+          className={`${showInfoRef3 ? 'fade-in-infoRight' : 'opacity-0'} mb-8 flex flex-col gap-6`}
         >
-          <h2 className="text-primary mr-4 text-right font-logo text-3xl md:mb-4 md:mr-12 md:text-5xl">
-            Just stopping by?
-          </h2>
-          <div className="md:grid md:grid-cols-1/3">
-            <div className="fade-in-right bg-secondary-2 col-start-2 ml-4 flex items-center gap-8 rounded-l-full p-4 shadow-md shadow-gray-300 md:gap-20 md:p-8 dark:shadow-black">
-              <ProfilePic className="size-48 h-full shadow-md shadow-gray-300 md:size-96 dark:shadow-black" />
-              <div>
-                <p className="text-secondary leading-normal">
-                  Sign in using the guest option to experience a fully featured
-                  version of the app
-                </p>
-                <p className="text-tertiary mt-2 text-sm md:mt-6 md:text-2xl">
-                  (Guest accounts are deleted on logout or after 4 hours)
-                </p>
-              </div>
+          <div
+            className={`bg-secondary-2 col-start-2 flex w-full flex-col items-start gap-4 self-end overflow-hidden rounded-xl p-4 shadow-md shadow-gray-300 md:w-3/4 md:p-8 dark:shadow-black`}
+          >
+            <div className="flex items-center gap-4 md:gap-8">
+              <PawIcon className="size-10 md:size-14" />
+              <h3 className="text-primary font-logo text-3xl md:mb-4 md:mr-12 md:text-4xl">
+                Just stopping by?
+              </h3>
+            </div>
+            <div className="bg-secondary rounded-lg p-2 text-xl shadow-md shadow-gray-300 md:p-4 md:text-2xl dark:shadow-black">
+              <p className="text-secondary leading-normal">
+                Sign in using the guest option to easily experience a fully
+                featured version of the app with no commitments!{' '}
+                <span className="text-tertiary mt-2 text-sm md:mt-6 md:text-2xl">
+                  (Guest accounts are automatically deleted on logout or after 4
+                  hours)
+                </span>
+              </p>
             </div>
           </div>
         </div>
--- src/components/LandingPageImage.jsx
+++ src/components/LandingPageImage.jsx
@@ -1,7 +1,7 @@
 const LandingPageImage = (props) => {
   return (
     <div
-      className={`${props.className} flex aspect-square shrink-0 items-center justify-center overflow-hidden rounded-full shadow-md shadow-gray-300 dark:shadow-black`}
+      className={`${props.className} flex aspect-square w-1/2 shrink-0 items-center justify-center overflow-hidden rounded-xl shadow-md shadow-gray-300 dark:shadow-black`}
       onClick={props.onClick}
     >
       <img className="min-h-full min-w-full object-cover" src={props.image} />
